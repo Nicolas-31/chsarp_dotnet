a808d3d5000997eae25c18c8c420e3e1c07b05ce
Nicolas Anderson <nicolas-31@outlook.com>
2023-04-16 07:56:43 +0200

    added project

---
+++ learning-csharp/SmallExercises/SmallExercises/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    // For loop - Factorial
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a positive integer number");
+        var userInput = int.Parse(Console.ReadLine());
+        Console.WriteLine("The factorial of entered number is " + Factorial(userInput));
+        Console.WriteLine("press any key to exist the program");
+        Console.ReadKey();
+    }
+
+
+    public static int Factorial(int number)
+    {
+        int factorial = 1; 
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+
+}
---
+++ learning-csharp/calculator/calculator/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("Hello!");
+                Console.WriteLine("Input the first number:");
+                double firstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Input the second number:");
+
+                double secondNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("What do you want to do?");
+
+                Console.WriteLine("[A]dd numbers");
+                Console.WriteLine("[S]ubstract numbers");
+                Console.WriteLine("[M]ultiply numbers");
+                Console.WriteLine("[D]ivide numbers");
+                Console.WriteLine("[Q]uit the program");
+
+                var operationChoice = Console.ReadLine();
+
+                if (EqualsCaseInsensitive(operationChoice, "A"))
+                {
+                    var sum = firstNumber + secondNumber;
+
+                    PrintFinalResult(firstNumber, secondNumber, sum, "+");
+                }
+                else if (EqualsCaseInsensitive(operationChoice, "S"))
+                {
+                    var difference = firstNumber - secondNumber;
+                    PrintFinalResult(firstNumber, secondNumber, difference, "-");
+                }
+                else if (EqualsCaseInsensitive(operationChoice, "M"))
+                {
+                    var multiplied = firstNumber * secondNumber;
+                    PrintFinalResult(firstNumber, secondNumber, multiplied, "*");
+                }
+                else if (EqualsCaseInsensitive(operationChoice, "D"))
+                {
+                    var divided = firstNumber / secondNumber;
+                    PrintFinalResult(firstNumber, secondNumber, divided, "/");
+                }
+                else if (EqualsCaseInsensitive(operationChoice, "Q"))
+                {
+                    run = false;
+                    Console.WriteLine("Press any key to close ");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice");
+                }
+            }
+        }
+
+        private static void PrintFinalResult(double firstNumber, double secondNumber, double result, string @operator)
+        {
+            Console.WriteLine(firstNumber + " " + @operator + " " + secondNumber + " = " + result);
+        }
+
+        private static bool EqualsCaseInsensitive(string left, string right)
+        {
+            return left.ToUpper() == right.ToUpper();
+        }
+    }
+   
+}
